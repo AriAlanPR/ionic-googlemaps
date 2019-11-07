0998eb52fa1389c2a77060aa31b83eec1b09c147
AriAlanPR <shaymin2003@gmail.com>
2019-11-06 22:30:35 -0600

    partially working google maps sdk on ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import {GoogleMaps, GoogleMap} from '@ionic-native/google-maps/ngx';
+import { Environment } from '@ionic-native/google-maps';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  map: GoogleMap;
 
+  constructor(private platform:Platform) {}
+
+  async ngOnInit(){
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap(){
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDZaNhEBjHTObPvzYI5dJoqGsUFK3EfxRE',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDZaNhEBjHTObPvzYI5dJoqGsUFK3EfxRE'
+    });
+
+    this.map = GoogleMaps.create('map_canvas');
+  }
 }
